447f48acdbe8b03ac085350a4c47f362b400517f
vairday <vairday@proton.me>
2023-04-05 08:20:46 -0500

    trying out yarn

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  trailingSlash: true,
   reactStrictMode: true,
   experimental: {
     appDir: true,
